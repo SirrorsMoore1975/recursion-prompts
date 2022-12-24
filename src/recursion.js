c2289b24e0b0f7b7f438241b71f87aa748ef5ca9
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2022-12-25 03:11:42 +0800

    WIP isEven(n)

--- src/recursion.js
+++ src/recursion.js
@@ -79,6 +79,23 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+    
+    if(n >= 2){
+        isEven(n - 2);
+    } else
+
+    if(n === 1){
+        return false;
+    } else
+
+    if(n === 0){
+        return true;
+    } else
+    
+    if(n < 0){
+        isEven(n * (-1));
+    } 
+    console.log(n);
 };
 
 // 5. Sum all integers below a given integer.
